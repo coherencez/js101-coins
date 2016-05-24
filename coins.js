b5dc39c1c5ef237a54479ae0a2ca51469d4aa595
Thomas Swann <tkswann@mac.com>
2016-05-24 00:12:20 -0500

    minor progress

--- coins.js
+++ coins.js
@@ -11,16 +11,22 @@
     nickels: 1,
     pennies: 2
   }
-
 */
 
-function coinCounter () {
+function coinCounter (x) {
   // Initialize a JavaScript object to hold the coins
-  var coinPurse = {};
+  var coinPurse = {}, quar = .25, dime = .10, nickel = .5, penn = .1;
+	coinPurse.quarters = 0;
+  coinPurse.dimes = 0;
+  coinPurse.nickels = 0;
+  coinPurse.pennies = 0;
+	coinPurse.myFunc = function () {
+		for (var i = 0, i < 100; i++) {
+			if ()
+		}
+	};
 
-  coinPurse.quarters = 0;
-
-  return coinPurse;
+	return coinPurse;
 }
 
 var coins = coinCounter()
